8652344b498b85cd0e283854124fd68c1c0105dd
Pedro Mello <pedrocmello.bsi@gmail.com>
2020-12-02 20:44:15 -0300

    iniciando projeto de todo app com o caio

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = (props) => {
+  
+  return (
+    <div>
+      {props.children}
+    </div>
+  )
+};
+
+export default Header;
---
+++ src/components/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Header>
+        <h1>Hello React World</h1>
+      </Header>
+      <div>
+        <h2>Eu sou o corpo da aplicação</h2>
+      </div>
+    </div>
+  )
+};
+
+export default Home;
